a9b47675f36fcee9cbed7ad8a8302e1f8bc70ca5
Pavel Rumkin <dev@rumk.in>
2019-03-29 21:09:03 +0300

    [src] Update indicator behaviour. Lift up styling

--- site.js
+++ site.js
@@ -1,6 +1,7 @@
 const indicator = document.getElementById('indicator');
 
 let timeout = 0;
+
 pill('#page', {
   onLoading() {
     if (timeout) {
@@ -8,11 +9,25 @@ pill('#page', {
       timeout = 0
     }
 
+    addClass(indicator, 'is-loading');
     indicator.style.display = 'block';
   },
   onReady() {
     timeout = setTimeout(() => {
+      removeClass(indicator, 'is-loading')
       indicator.style.display = 'none';
-    }, 1000)
+    }, 2000)
   }
 })
+
+function addClass(target, className) {
+  target.className = target.className.trim().split(/\s+/)
+  .concat(className)
+  .join(' ');
+}
+
+function removeClass(indicator, className) {
+  indicator.className = indicator.className.trim().split(/\s+/)
+  .filter(item => item !== className)
+  .join(' ');
+}
