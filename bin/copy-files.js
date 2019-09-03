37b7a08436c7d7ea5b6d2a83c59db8d8d9ab4769
Pavel Rumkin <dev@rumk.in>
2019-09-03 05:08:46 +0300

    [util] Fix glob matchin issue

--- bin/copy-files.js
+++ bin/copy-files.js
@@ -1,3 +1,3 @@
 const copy = require('copy')
 
-copy('dist/pill.js', 'example', () => console.log('copied pill.js build into example'))
+copy('dist/*.js', 'example', () => console.log('copied pill.js build into example'))
