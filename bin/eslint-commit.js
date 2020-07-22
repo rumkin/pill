fc08b91dd43474617fc8f4bebaef9fedc2f57c37
Paul Rumkin <dev@rumk.in>
2020-07-23 01:43:52 +0300

    [deps,util] Add ESLint checks on commits

---
+++ bin/eslint-commit.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const {execSync: exec} = require('child_process')
+const {ESLint} = require('eslint')
+
+async function main() {
+  // Load stashed git files
+  const files = exec('git diff --cached --name-status')
+  .toString('utf8')
+  .split('\n')
+  .filter(s => /^(M|A)/.test(s))
+  .filter(s => /\.js$/.test(s))
+  .map(s => s.slice(2))
+
+  if (! files.length) {
+    return 0
+  }
+
+  // Create linter instance
+  const eslint = new ESLint()
+  // Create formatter instance
+  const formatter = await eslint.loadFormatter('stylish')
+
+  // Lint files
+  let hasErrors = false
+  for (const file of files) {
+    const code = exec(`git show :${file}`).toString('utf8')
+    const result = await eslint.lintText(code, {
+      filePath: file,
+    })
+
+    if (result[0].errorCount > 0) {
+      hasErrors = true
+    }
+
+    const report = formatter.format(result)
+    if (report.length) {
+      console.log(report)
+    }
+  }
+
+  if (hasErrors === true) {
+    return 1
+  }
+  else {
+    return 0
+  }
+}
+
+main()
+.catch(error => {
+  console.error(error)
+
+  return 1
+})
+.then((code = 0) => process.exit(code))
