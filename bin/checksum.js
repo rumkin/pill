54b3650cdc4183bfd26ddf2fe0f85cbad36b5498
Paul Rumkin <dev@rumk.in>
2020-12-01 07:38:57 +0300

    [lint] Fix checksum util

--- bin/checksum.js
+++ bin/checksum.js
@@ -4,8 +4,8 @@ const {createHash} = require('crypto')
 
 function calcHash(filepath) {
   const hash = createHash('sha384')
-
   const stream = fs.createReadStream(filepath)
+
   return new Promise((resolve, reject) => {
     stream.on('data', (chunk) => hash.update(chunk))
     stream.on('error', reject)
@@ -17,12 +17,12 @@ function calcHash(filepath) {
 
 async function calcHashes(dir, files) {
   dir = path.resolve(dir)
-  const result = await Promise.all(files.map(async (p) => {
-    const filepath = path.resolve(p)
-    const digest = await calcHash(filepath)
+  const result = await Promise.all(files.map(async (relPath) => {
+    const absPath = path.resolve(relPath)
+    const digest = await calcHash(absPath)
 
     return {
-      filepath: path.relative(dir, filepath),
+      filepath: path.relative(dir, absPath),
       digest,
     }
   }))
@@ -35,6 +35,7 @@ async function calcHashes(dir, files) {
 calcHashes(process.argv[2], process.argv.slice(3))
 .catch((error) => {
   console.error(error)
+
   return 1
 })
 .then((code = 0) => process.exit(code))
